72b5c8ba28f2d2facadcf2d408b157a10816ddf9
230105 <230105@hkis.edu.hk>
2018-03-08 19:10:10 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -49,7 +49,7 @@ function create(){
 	player.animation.add('right', [0,1,2,3],10,true)
 	game.physics.arcade.enable(player);
 	player.body.gravity.y = 300;
-	player.body.bounce/y = 0.2;
+	player.body.bounce.y = 0.2;
 	player.body.collideWorldBounds = true;
 
 
