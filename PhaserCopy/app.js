80f71dde02c250b26b9ff4617859915e6e68c4b5
230105 <230105@hkis.edu.hk>
2018-03-14 18:05:31 +0800

    debug

--- PhaserCopy/app.js
+++ PhaserCopy/app.js
@@ -5,10 +5,10 @@ var lives = 3;
 
 function preload(){
 	game.load.image('sky', 'assets/sky.png');
-	game.load.image('ground', 'assets/platforms.png');
+	game.load.image('ground', 'assets/platform.png');
 	game.load.image('star', 'assets/star.png');
-	game.load.spritesheet('dude', 'assets/dude',32,48);
-	game.load.spritesheet('baddie', 'assets/baddie',32,32 );
+	game.load.spritesheet('dude', 'assets/dude.png',32,48);
+	game.load.spritesheet('baddie', 'assets/baddie.png',32,32 );
 }
 
 function create(){
